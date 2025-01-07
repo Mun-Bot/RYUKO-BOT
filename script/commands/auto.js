fcf0267dd61b53207d74e317d74667a65173d643
serverbot81 <serverbot81@gmail.com>
2025-01-07 16:22:47 +0600

    Update auto.js

--- script/commands/auto.js
+++ script/commands/auto.js
@@ -27,11 +27,11 @@ const data = await alldown(content);
   const video = (await axios.get(high, {
       responseType: "arraybuffer",
     })).data;
-    fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"))
+    fs.writeFileSync(__dirname + "/system/auto.mp4", Buffer.from(video, "utf-8"))
 
         return api.sendMessage({
             body: `[🤍] 𝘚𝘵𝘢𝘺 𝘞𝘪𝘵𝘩 𝘛𝘢𝘯𝘷𝘪𝘳 𝘉𝘰𝘵 🥀`,
-            attachment: fs.createReadStream(__dirname + "/cache/auto.mp4")
+            attachment: fs.createReadStream(__dirname + "/system/auto.mp4")
 
         }, event.threadID, event.messageID);
     }
