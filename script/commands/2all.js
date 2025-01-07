06bbbef1f2208d298b513f2f8b27d19b9767bf1e
serverbot81 <serverbot81@gmail.com>
2025-01-07 15:40:02 +0600

    Create 2all.js

---
+++ script/commands/2all.js
@@ -0,0 +1,30 @@
+module.exports.config = {
+    name: "2all",
+    version: "1.0.0",
+    hasPermssion: 2,
+    credits: "NoMore",
+    description: "X",
+    commandCategory: "user",
+    usages: "@mention",
+    cooldowns: 20,
+    dependencies: {
+        "fs-extra": "",
+        "axios": ""
+    }
+}
+
+module.exports.run = async function({ api, args, Users, event}) {
+    var mention = Object.keys(event.mentions)[0];
+    if(!mention) return api.sendMessage("T mention", event.threadID);
+ let name =  event.mentions[mention];
+    var arraytag = [];
+        arraytag.push({id: mention, tag: name});
+    var a = function (a) { api.sendMessage(a, event.threadID); }
+a("Try");
+setTimeout(() => {a({body: "❎" + " " + name, mentions: arraytag})}, 3000);
+setTimeout(() =>"Unexpected")} , 3010);
+
+
+
+  
+  }
