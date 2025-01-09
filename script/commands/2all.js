86c08b375447edb2a489e4775f0f9de57cc24130
serverbot81 <serverbot81@gmail.com>
2025-01-09 14:10:32 +0600

    Update 2all.js

--- script/commands/2all.js
+++ script/commands/2all.js
@@ -1,76 +1,36 @@
-const axios = require("axios");
-const fs = require("fs");
-
-const ok = "xyz";
-const apiBaseUrl = `https://smfahim.${ok}/sing`;
-
 module.exports = {
-  config: {
-    name: "singx",
-    version: "1.4",
-    aliases: ["song"],
-    author: "Team Calyx",
-    cooldowns: 5,
+	config: {
+		name: "all",
+		version: "1.2",
+		credits: "NTKhang",
+		cooldowns: 5,
+		permission: 1,
     prefix: true,
-    permission: 2,
-    category: "media",
-  },
+		description: "Tag all members in your group chat",
+		category: "box chat"
+	},
 
-  run: async ({ api, args, event }) => {
-    let videoURL = args.join(" ");
-
-    if (!videoURL) {
-      if (event.messageReply && event.messageReply.body) {
-        const urlRegex = /(https?:\/\/[^\s]+)/g;
-        const foundURLs = event.messageReply.body.match(urlRegex);
-        if (foundURLs && foundURLs.length > 0) {
-          videoURL = foundURLs[0];
-        } else {
-          return api.sendMessage("❌ No URL found in the replied message. Please provide a valid URL.", event.threadID, event.messageID);
-        }
-      } else {
-        return api.sendMessage("❌ Please provide a search term or URL.", event.threadID, event.messageID);
-      }
-    }
-
-    const isUrl = videoURL.startsWith("http://") || videoURL.startsWith("https://");
-
-    try {
-      api.setMessageReaction("⏳", event.messageID, () => {}, true);
-
-      let response;
-      if (isUrl) {
-        response = await axios.get(`${apiBaseUrl}?url=${encodeURIComponent(videoURL)}`);
-      } else {
-        response = await axios.get(`${apiBaseUrl}?search=${encodeURIComponent(videoURL)}`);
-      }
-
-      const { link, title } = response.data;
-
-      if (!link) {
-        api.setMessageReaction("❌", event.messageID, () => {}, true);
-      }
-
-      const audioPath = `ytb_audio_${Date.now()}.mp3`;
-
-      const audioResponse = await axios.get(link, { responseType: "arraybuffer" });
-      fs.writeFileSync(audioPath, Buffer.from(audioResponse.data));
-      api.setMessageReaction("✅", event.messageID, () => {}, true);
-      const titleBody = `• Title: ${title}`;
-      await api.sendMessage(
-        {
-          body: titleBody,
-          attachment: fs.createReadStream(audioPath)
-        },
-        event.threadID,
-        () => {
-          fs.unlinkSync(audioPath);
-        },
-        event.messageID
-      );
-    } catch (error) {
-      console.error(error);
-      api.setMessageReaction("❌", event.messageID, () => {}, true);
-    }
-  }
+	run: async function ({ message, event, args }) {
+		const { participantIDs } = event;
+		const lengthAllUser = participantIDs.length;
+		const mentions = [];
+		let body = args.join(" ") || "@mga_tae";
+		let bodyLength = body.length;
+		let i = 0;
+		for (const uid of participantIDs) {
+			let fromIndex = 0;
+			if (bodyLength < lengthAllUser) {
+				body += body[bodyLength - 1];
+				bodyLength++;
+			}
+			if (body.slice(0, i).lastIndexOf(body[i]) != -1)
+				fromIndex = i;
+			mentions.push({
+				tag: body[i],
+				id: uid, fromIndex
+			});
+			i++;
+		}
+		message.reply({ body, mentions });
+	}
 };
