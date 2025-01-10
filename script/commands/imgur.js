17d789d4402412376cfe0a7532e4f4851b5a4f43
serverbot81 <serverbot81@gmail.com>
2025-01-10 14:20:48 +0600

    Update imgur.js

--- script/commands/imgur.js
+++ script/commands/imgur.js
@@ -15,7 +15,8 @@ module.exports.config = {
 module.exports.run = async ({ api, event }) => {
 const axios = global.nodemodule['axios'];  
 var tanvir143 = event.messageReply.attachments[0].url || args.join(" ");
-	if(!tanvir143) return api.sendMessage('TX test', event.threadID, event.messageID)
+if (!event.messageReply || !event.messageReply.attachments || !event.messageReply.attachments[0]) {
+      return api.sendMessage("𝘱𝘭𝘦𝘢𝘴𝘦 𝘳𝘦𝘱𝘭𝘢𝘺 𝘵𝘩𝘦 𝘪𝘮𝘢𝘨𝘦.", event.threadID, event.messageID);	
 const res = await axios.get(`https://kaiz-apis.gleeze.com/api/imgur?url=${encodeURIComponent(tanvir143)}`);    
 var tanvir = res.data.uploaded.image;
     return api.sendMessage(`${tanvir}`, event.threadID, event.messageID);
